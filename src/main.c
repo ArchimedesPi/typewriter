c450f0ab3d3872a7f3074621556c9ef3293c642a
Liam <liam@cpan.org>
2017-01-15 23:09:51 -0600

    Begin wiring tw_* into main.c

--- src/main.c
+++ src/main.c
@@ -7,10 +7,49 @@
 #include "config.h"
 #include "util.h"
 #include "uart.h"
+#include "typewriter.h"
+
+tw_state_t tw_state;
 
 int main() {
 	sei(); // turn on interrupts
 	uart0_init(UART_BAUD_SELECT(BAUD, F_CPU));
+
+	/* Initialize the typewriter. (Slews motors!) */
+	tw_init(&tw_state);
+	
+	while (true) {
+		if (uart0_available() > 0) {
+			char c = uart0_getc();
+			switch (c) {
+			case 0x05: { /* ENQ */
+				uart0_putc(0x06);
+				break;
+			}
+
+			case '\r': { /* Carriage return */
+				tw_carriage_return(&tw_state);
+				break;
+			}
+
+			case '\n': { /* Newline */
+				tw_newline(&tw_state);
+				break;
+			}
+
+			case '\b': { /* Backspace */
+				break;
+			}
+
+			case 0x1B: { /* Escape */
+				break;
+			}
+
+			default: /* Print character */
+				break;
+			}
+		}
+	}
 
 	return 0;
 }
