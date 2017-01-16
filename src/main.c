86512b564e4bd3904ab55642d828dd45c7477d21
Liam <liam@cpan.org>
2017-01-15 23:04:33 -0600

    main.c: Integrate avr-uart

--- src/main.c
+++ src/main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
 
 #include "util.h"
 #include "uart.h"
 
 int main() {
-	uart_init();
-	stdin = &uart0in;
-	stdout = &uart0out;
+	sei(); // turn on interrupts
+	uart0_init(UART_BAUD_SELECT(BAUD, F_CPU));
 
 	return 0;
 }
