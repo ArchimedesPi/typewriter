ff8e8fbf45e58e2704878d6d10d678bc1f53b430
Liam <liam@cpan.org>
2017-01-17 18:29:43 -0600

    Rewrite pin abstraction layer

--- src/pin.h
+++ src/pin.h
@@ -1,6 +1,19 @@
 #pragma once
 #include "util.h"
+#include <stdbool.h>
 
-#define PIN(x) u8* x##_port; const u8 x##_pin
+typedef struct _pin_t {
+	volatile u8 *r_port;
+	volatile u8 *r_ddr;
+	volatile u8 *r_pin;
+	u8 pin;
+} _pin_t;
 
-#define SET_PIN(x, v) *x##_port ^= (-v ^ *x##_port) & _BV(x##_pin);
+#define _def_pin(po, pi) (_pin_t){.r_port=&PORT##po,.r_ddr=&DDR##po,.r_pin=&PIN##po,.pin=pi}
+#define def_pin(po, pi) _def_pin(po, pi)
+
+#define set_pin(x, v) *x.r_port ^= (-v ^ *x.r_port) & _BV(x.pin)
+#define get_pin(x) (*x.r_pin & _BV(x.pin))
+#define set_output(x) *x.r_ddr |= _BV(x.pin)
+#define set_input(x) *x.r_ddr &= ~_BV(x.pin)
+#define set_input_pullup(x) set_input(x); set_pin(x, 1)
