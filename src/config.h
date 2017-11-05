9f54576bfef48e8c05fb28716b04b7cfdb181a08
Liam <liam@cpan.org>
2017-11-05 10:02:42 -0600

    bump config.h

--- src/config.h
+++ src/config.h
@@ -1,7 +1,8 @@
 #pragma once
 
+#define DEBUG
 #define BAUD 9600
 
 #define ROLL_SP_LINE 20 // TODO: measure real value
-#define CARRIAGE_SP_COL 20 // TODO: measure real value
-#define DAISYWHEEL_SP_CHAR 20 // TODO: measure/calculate real value
+#define CARRIAGE_SP_COL 10 // TODO: measure real value
+#define DAISYWHEEL_SP_CHAR 1 // TODO: measure/calculate real value
